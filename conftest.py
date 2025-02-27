fd68f408f9d26c91c5b56ecd2b1b25d6ad5d73b9
Christian Clauss <cclauss@me.com>
2025-02-27 10:08:30 +0100

    Xfail pytests that raise an OpenAIError because of a missing OPENAI_API_KEY

--- conftest.py
+++ conftest.py
@@ -1,6 +1,8 @@
 import os
 import sys
 
+import pytest
+
 from browser_use.logging_config import setup_logging
 
 # Get the absolute path to the project root
@@ -8,3 +10,16 @@ project_root = os.path.dirname(os.path.abspath(__file__))
 sys.path.insert(0, project_root)
 
 setup_logging()
+
+
+class OpenAIError(Exception):
+    """Custom Exception to simulate a missing OPENAI_API_KEY."""
+    pass
+
+
+def pytest_runtest_call(item):
+    """Xfail pytests that raise an OpenAIError because of a missing OPENAI_API_KEY."""
+    try:
+        item.runtest()
+    except OpenAIError:
+        pytest.xfail(f"Test {item.name} ignored due to a missing OPENAI_API_KEY.")
